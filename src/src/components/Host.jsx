58a48a391a8907dcd57396b2bb1d884615d445e1
LMgit91 <163318506+LMgit91@users.noreply.github.com>
2024-09-19 14:19:30 +0200

    Delete src/src directory

--- src/src/components/Host.jsx
+++
@@ -1,7 +0,0 @@
-import '../Styles/locationList.scss';
-export default function Host({dataStorage}){
-    return(<div className="location_name_bloc2">
-        <p className="location_name_bloc2_p">{dataStorage && dataStorage.host.name}</p>
-        <div> <img className="picture_ownner" src={`${dataStorage.host.picture}`} alt={`${dataStorage.host.name}`}/></div>
-    </div>)
-}
--- src/src/components/NameInfo.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function NameInfo({dataStorage}){
-    return(
-        <div className="location_name_bloc1">
-            <p className="detail_aboutlocation">{dataStorage && dataStorage.title}</p>
-            <p className="localisation_detail">{dataStorage && dataStorage.location}</p>
-        </div>
-    )
-}
