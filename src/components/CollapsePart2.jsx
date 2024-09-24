9b97578b4b63441e38b0fc76f7807b81cf08ac90
LMgit91 <163318506+LMgit91@users.noreply.github.com>
2024-09-24 17:09:42 +0200

    Add files via upload

--- src/components/CollapsePart2.jsx
+++ src/components/CollapsePart2.jsx
@@ -1,20 +1,46 @@
 import '../Styles/navBar.scss';
 import '../Styles/propos.scss';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faAngleUp} from "@fortawesome/free-solid-svg-icons";
-//className={`${showText ? "visibility" : "non_visibility"}`}
+
 
 export default function CollapseItem({title, content}){
     
     const[showText, setShowText] = useState(false);
+    const [heightCollapse, setHeightCollapse] = useState();
+
+    const variableHeight = useRef()
+
+    useEffect(() => {
+        setHeightCollapse(`${variableHeight.current.scrollHeight}px`)
+    }, [])
 
     const funcVisibility = () => {
         setShowText(item => !item)
     }
 
-    return(<div>
-                <div className='box2'><span>{title}</span><span className='collapseSpan' onClick={funcVisibility}><FontAwesomeIcon  style={showText ? {'transform' : 'rotate(180deg)', 'transition': '0.5s'} : {'transition': '0.5s'}}  icon={faAngleUp} /></span></div>
-                <p style={showText ? {'minHeight' : '20px', 'overflow' : 'visible'} : {'overflow' : 'hidden', 'maxHeight': '0'}} className={`para_modified`}>{content}</p>
-            </div>)
+    return (
+        <div className="box1">
+
+            <button 
+            onClick={funcVisibility}
+            className="box2">
+                <span>{title}</span>
+                <FontAwesomeIcon className='iconStyle' style={showText ? {'transform' : 'rotate(180deg)', 'transition': '0.5s'} : {'transition': '0.5s'}} icon={faAngleUp} />
+            </button>
+            
+            <div 
+            className={showText ? "para_modified animated" : "para_modified"}
+            style={{height: showText ? `${heightCollapse}` : "0px"}}
+            ref={variableHeight}
+            >
+                <p aria-hidden={showText ? "true" : "false"}>
+                    {content}
+                </p>
+            </div>
+            
+        </div>
+    )
+
 }
